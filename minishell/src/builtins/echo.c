a798387ba4646c6d71804e2f03e3e7192cd7165c
davmendo <davmendo@student.42porto.com>
2025-11-05 21:49:07 +0000

    echo.c -n

--- minishell/src/builtins/echo.c
+++ minishell/src/builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leoaguia <leoaguia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: davmendo <davmendo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 22:52:38 by leoaguia          #+#    #+#             */
-/*   Updated: 2025/11/02 22:52:39 by leoaguia         ###   ########.fr       */
+/*   Updated: 2025/11/05 21:43:09 by davmendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,37 @@ int	builtin_echo(char **argv)
 {
 	int	i;
 	int	nl;
+	int	first_arg;
+	int	j;
 
 	i = 1;
 	nl = 1;
-	if (argv[1] && !strcmp(argv[1], "-n"))
+	while (argv[i] && argv[i][0] == '-' && argv[i][1])
 	{
+		/* comeca em 1 para pular o sinal '-' */
+		j = 1;
+		/* consome todos os 'n' consecutivos */
+		while (argv[i][j] == 'n')
+			j++;
+		/* se apos o 'n' sobrar algo nao e da flag... */
+		if (argv[i][j] != '\0')
+			break ;
 		nl = 0;
-		i = 2;
+		/* avanca para o proximo argumetno */
+		i++;
 	}
+	/* guarda o indice do primeiro agrumento sem ser a flag */
+	first_arg = i;
 	while (argv[i])
 	{
-		if (i > (nl ? 1 : 2)) write(1, " ", 1);
+		/* se nao for o primeiro argumento a imprimir, escvere um espaco antes do primeiro argumetno */
+		if (i > first_arg)
+			write(1, " ", 1);
 		write(1, argv[i], strlen(argv[i]));
 		i++;
 	}
-	if (nl) write(1, "\n", 1);
+	/*se nao foi encontrado nenhum flag -n imprime uma newline */
+	if (nl)
+		write(1, "\n", 1);
 	return (0);
 }
