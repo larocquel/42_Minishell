61672fc083ea0fd69622118108fe31f001edd697
larocquel <leonardolarocque@gmail.com>
2025-11-05 23:38:06 +0000

    Fixed tokens and added mask (missing norminette)

--- minishell/inc/minishell.h
+++ minishell/inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: leoaguia <leoaguia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 22:51:19 by leoaguia          #+#    #+#             */
-/*   Updated: 2025/11/04 18:56:02 by leoaguia         ###   ########.fr       */
+/*   Updated: 2025/11/05 23:33:00 by leoaguia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,15 @@
 # include <fcntl.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+/*	MACROS	*/
+
+# define NQ 0
+# define SQ 1
+# define DQ 2
+
+
+/*	TYPES	*/
 
 extern volatile sig_atomic_t	g_sig;
 
@@ -48,37 +57,65 @@ typedef struct s_token
 {
 	t_toktype		type;
 	char			*val;
+	unsigned char	*qmask;
 	struct s_token	*next;
 }	t_token;
 
-/* core */
+typedef struct s_str
+{
+	char	*data;
+	size_t	cap;
+	size_t	len;
+}	t_str;
+
+typedef struct s_mask
+{
+	unsigned char	*data;
+	size_t			cap;
+	size_t			len;
+}	t_mask;
+
+//	CORE
 void	shell_init(t_shell *sh, char **envp);
 void	shell_destroy(t_shell *sh);
 void	repl_loop(t_shell *sh);
 
-/* signals */
+//	SIGNALS
 void	signals_setup_interactive(void);
 void	signals_setup_child(void);
 void	sigint_handler(int signo);
 
-/* simple parser + future lexer */
+//	SSIMPLE PARSER + FUTURE LEXER
 char	**tokenize_simple(const char *line);
 void	free_tokens(char **toks);
 t_token	*lex_line(const char *s, int *err);
 void	free_token_list(t_token *lst);
 char	**tokens_to_argv(t_token *lst);
 
-/* tokenizer */
+//	TOKENS
+//	token.c
 t_token	*lex_line(const char *s, int *err);
 void	free_token_list(t_token *lst);
+//	token_utils.c
+int	tk_is_space(int c);
+int	tk_is_operator(int c);
+t_token	*tk_new(t_toktype type, char *val, unsigned char *mask);
+void	tk_push(t_token **head, t_token **tail, t_token *node);
+//	token_read.c
+t_token	*tk_read_op(const char *s, size_t *i);
+char	*tk_read_word(
+	const char *s, size_t *i, int *err, unsigned char **out_mask);
+//	token_buf.c
+char	*tk_add_str(t_str *b, char c, int *err);
+unsigned char	*tk_add_mask(t_mask *m, unsigned char v, int *err);
 
-/* exec */
+//	EXEC
 int		is_builtin(const char *cmd);
 int		exec_builtin(t_shell *sh, char **argv);
 int		exec_external(t_shell *sh, char **argv);
 char	*ms_search_path(char **envp, const char *cmd);
 
-/* utils */
+//	UTILS
 size_t	ms_split_count(char **arr);
 char	*ms_strjoin3(const char *a, const char *b, const char *c);
 char	**ms_env_dup(char **envp);
@@ -88,7 +125,7 @@ int		ms_setenv(t_shell *sh, const char *k, const char *v);
 int		ms_unsetenv(t_shell *sh, const char *key);
 int		ms_isnumber(const char *s);
 
-/* builtins */
+//	BUILTINS
 int		builtin_echo(char **argv);
 int		builtin_pwd(void);
 int		builtin_env(t_shell *sh);
